00bc0736eaa7fe3b7abee54eaf5d5b687e4681fa
Renjith Raj <renjithraj2005@gmail.com>
2020-06-26 23:43:31 +0530

    logs added for the listner

--- src/main/java/com/demo/batch/JobCompletionNotificationListener.java
+++ src/main/java/com/demo/batch/JobCompletionNotificationListener.java
@@ -27,6 +27,7 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
     public void afterJob(JobExecution jobExecution) {
         if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("!!! JOB FINISHED! Time to verify the results");
+            log.info("Start Time : "+jobExecution.getStartTime());
 
             jdbcTemplate.query("SELECT first_name, last_name FROM people",
                     (rs, row) -> new Person(
